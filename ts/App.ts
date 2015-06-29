6a180bd8d6e6b44ca507f3721847185beaa5e505
plesin <info@radekpleskac.com>
2015-06-29 19:50:30 +0200

    adding initial files

---
+++ ts/App.ts
@@ -0,0 +1,9 @@
+module PE {
+	export class App {
+		constructor() {	}
+		
+		init():void {
+			console.log("initialized");
+		}
+	}	
+}
---
+++ ts/main.ts
@@ -0,0 +1,6 @@
+declare var app:PE.App;
+
+document.addEventListener("DOMContentLoaded", (event:Event) => {
+	app = new PE.App();
+	app.init();
+});
