12e3a26e262a1fd23de43caee48425f0f49b08e5
plesin <info@radekpleskac.com>
2015-07-02 18:34:10 +0200

    fit images to viewport

--- app/ts/App.ts
+++ app/ts/App.ts
@@ -1,7 +1,11 @@
 module PE {
 	export class App {
 		canvas: HTMLCanvasElement;
-		constructor() {	}
+		view: any;
+		
+		constructor() {
+			this.view = new View();	
+		}
 		
 		init():void {
 			this.canvas = <HTMLCanvasElement>document.querySelector("canvas");
@@ -19,13 +23,19 @@ module PE {
 		    var reader = new FileReader();
 		    reader.onload = (event:any) => {
 		        var img = new Image();
-		        img.onload = () => {
-					var context = <CanvasRenderingContext2D>this.canvas.getContext("2d");		            
-		            context.drawImage(img, 0, 0);
+		        img.onload = () => {					
+					var canvasSize = this.view.getImageSizeToViewPort(img);
+					console.log(canvasSize);	
+					this.canvas.width = canvasSize.width;
+					this.canvas.height = canvasSize.height;
+					var context = <CanvasRenderingContext2D>this.canvas.getContext("2d");
+					//ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvasSize.width, canvasSize.height);				
+					context.drawImage(img, 0, 0, canvasSize.width, canvasSize.height);
 		        }
 		        img.src = event.target.result;
 		    }
 		    reader.readAsDataURL(evt.target.files[0]);     
 		}
+				
 	}	
 }
