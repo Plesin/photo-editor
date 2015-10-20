ff1862a94276144435bd061e42031382b04a5d05
Radek Pleskac <info@radekpleskac.com>
2015-10-20 21:49:16 +0200

    fixed platform regexp

--- scripts/open.js
+++ scripts/open.js
@@ -1,4 +1,3 @@
 var opn = require("opn");
-var chromeName = process.platform.match(/win/) ? "chrome" : "google chrome";
- 
+var chromeName = process.platform.match(/^win/) ? "chrome" : "google chrome"; 
 opn("http://localhost:9191", { "app" : chromeName });
